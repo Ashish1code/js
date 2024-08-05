c874424264d7aa1c886e196ccdedf6cffb8d39e9
Ashish1code <shashiashishrai@gmail.com>
2024-08-05 02:00:41 +0000

    typeofdatatype

---
+++ 01_basics/typeofdatatype.js
@@ -0,0 +1,26 @@
+//primitive type
+// string boolean number symbol null undefiend BigInt
+
+const MyString="Ashish"
+const MyBoolean=true
+const MyNumber=456
+const MySymbol=Symbol('123')
+const Mynull=null
+const MyUndefiend=undefined
+
+//Non Primitive 
+// Array Objact Function
+
+const MyArray=[1,2,3]
+const MyObject={
+    name:'ashish',
+    age:25,
+}
+const MyFunction=function add(){
+    console.log(2+2)
+}
+
+console.log(MyFunction)
+
+console.log(typeof MyFunction)
+
